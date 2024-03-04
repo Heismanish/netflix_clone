fc4df69823c78362d97828cff2e6f041fdecd89d
Manish Kumar Gupta <manishgu231@gmail.com>
2024-03-04 23:49:06 +0530

    Initial Commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function auth() {
+  return (
+    <div className="realtive h-full w-full bg-[url('/hero.jpg')] bg-no-repeat">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-"></nav>
+      </div>
+    </div>
+  );
+}
+
+export default auth;
