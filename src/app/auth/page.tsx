a1ce7647b4b605484cf2e94b354eb1c61c83ed8d
Manish Kumar Gupta <manishgu231@gmail.com>
2024-03-05 23:59:07 +0530

    auth input designed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import Input from "@/Components/Input";
+import Image from "next/image";
 
 function auth() {
   return (
     <div className="realtive h-full w-full bg-[url('/hero.jpg')] bg-no-repeat">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-"></nav>
+        <nav className="px-12 py-6">
+          <Image
+            src="/logo.png"
+            width={160}
+            height={120}
+            className="h-12"
+            alt="netflix logo"
+          />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black/70 px-16 py-16 self-center">
+            <Input></Input>
+          </div>
+        </div>
       </div>
     </div>
   );
